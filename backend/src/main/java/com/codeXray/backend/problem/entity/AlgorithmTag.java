fc6931cb34a42f581195147914035c0690090ff7
tmakdrl <tmakdrl@naver.com>
2026-08-05 11:33:53 +0900

    @BatchSize 추가

--- backend/src/main/java/com/codeXray/backend/problem/entity/AlgorithmTag.java
+++ backend/src/main/java/com/codeXray/backend/problem/entity/AlgorithmTag.java
@@ -5,11 +5,14 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.BatchSize;
 
 @Entity
 @Table(name = "algorithm_tags")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+// 여러 ProblemTag가 참조하는 태그를 개별 조회하면 N+1 → 최대 100개까지 IN 절로 묶어 로딩
+@BatchSize(size = 100)
 public class AlgorithmTag {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
